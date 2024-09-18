73ffa1bb2d89cec717eead4b20475547c484437c
AmolSonawane1026 <142985680+AmolSonawane1026@users.noreply.github.com>
2024-09-18 14:25:23 +0530

    added another video to main background

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import "./Main.css"
-import BgVideo from "../../assets/Videos/BgVideo3.mp4"
+import React from "react";
+import "./Main.css";
+import BgVideo from "../../assets/Videos/BgVideo1.mp4";
 const Main = () => {
   return (
     <>
-<main className='main-div'>
-<div className="overlay"></div>
+      <main className="main-div">
+        <div className="overlay"></div>
         <video autoPlay loop muted className="background-video">
           <source src={BgVideo} type="video/mp4" />
           Your browser does not support the video tag.
@@ -14,9 +14,9 @@ const Main = () => {
           <h1>Your Text Here</h1>
           <p>Some description or subtext can go here.</p>
         </div>
-</main>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
